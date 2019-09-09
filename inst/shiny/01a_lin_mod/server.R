2f40e10c400577af675f69d478772bbac24fb532
GuyliannEngels <guyliann.engels@umons.ac.be>
2019-09-09 09:33:00 +0200

    shiny app sur le modèle linéaire 2 version

--- inst/shiny/01a_lin_mod/server.R
+++ inst/shiny/01a_lin_mod/server.R
@@ -16,10 +16,36 @@ shinyServer(function(input, output) {
     trees <- data.io::read("trees", package = "datasets", lang = "FR") %>.%
         dplyr::select(., -height)
 
+    xvar <- trees$diameter
+    yvar <- trees$volume
+
     output$trees_plot <- renderPlot({
 
         chart::chart(trees, volume ~ diameter) +
             ggplot2::geom_point() +
             geom_abline(aes(slope = input$slope_ui, intercept = input$intercept_ui))
     })
+
+    output$resid_sum <- renderText({
+            predict <- xvar * input$slope_ui + input$intercept_ui
+            dist <- yvar - predict
+            value <- sum(dist)
+            print(value)
+    })
+
+    output$resid2_sum <- renderText({
+            predict <- xvar * input$slope_ui + input$intercept_ui
+            dist <- yvar - predict
+            value <- sum(dist ^ 2)
+            print(value)
+    })
+
+    output$resid_abs_sum <- renderText({
+            predict <- xvar * input$slope_ui + input$intercept_ui
+            dist <- yvar - predict
+            value <- sum(abs(dist))
+            print(value)
+    })
+
+
 })
--- inst/shiny/01a_lin_mod/ui.R
+++ inst/shiny/01a_lin_mod/ui.R
@@ -21,7 +21,31 @@ shinyUI(fluidPage(
         mainPanel(
           h4("Ajustez aux mieux la droite dans ce nuage de points"),
           p("Nous pouvons traduire cette formule pour notre cas concret $$Volume = pente \\times diametre \\ + \\ ordonnee \\ à \\ l \\ origine$$ "),
-          plotOutput("trees_plot")
-        )
-    )
-))
+          plotOutput("trees_plot"),
+          hr(),
+          h4("Tentez de minimiser les valeurs ci-dessous"),
+          p("Somme des résidus"),
+          verbatimTextOutput("resid_sum"),
+          p("Somme des carrés des résidus"),
+          verbatimTextOutput("resid2_sum"),
+          p("Somme des valeurs absolues des résidus"),
+          verbatimTextOutput("resid_abs_sum"),
+          hr()#,
+          #  column(3,
+          #    wellPanel(
+          #     p("Somme des résidus"),
+          #     verbatimTextOutput("resid_sum")
+          #   )),
+          # column(3,
+           #   wellPanel(
+           #     p("Somme des carrés des résidus"),
+           #     verbatimTextOutput("resid2_sum")
+           #   )),
+          #  column(3,
+         #     wellPanel(
+          #      p("Somme des valeurs absolues des résidus"),
+           #     verbatimTextOutput("resid_abs_sum")
+              )
+      )
+  )
+  )
